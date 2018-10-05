de338fcd40565f66e80c7b1a5132ee517dd7854d
Eldon Dahlin <end1989@gmail.com>
2018-10-04 21:23:18 -0600

    Pet Clinic in Spring all initalized and started

---
+++ src/main/java/guruspring/petclinicspring/PetClinicSpringApplication.java
@@ -0,0 +1,12 @@
+package guruspring.petclinicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicSpringApplication.class, args);
+    }
+}
